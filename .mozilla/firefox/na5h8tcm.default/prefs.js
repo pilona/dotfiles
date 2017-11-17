6cf9fd0eea2db1a8285b7173dd135cc14a9024a1
Alex Pilon <alp@alexpilon.ca>
2017-11-17 09:21:01 -0500

    .mozilla/firefox: Configure new tab preferences
    This is brittle, and you're not supposed to touch it, but it works. Can
    always remove it if it breaks.

---
+++ .mozilla/firefox/na5h8tcm.default/prefs.js
@@ -0,0 +1,4 @@
+user_pref("browser.bookmarks.restore_default_bookmarks", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
+user_pref("browser.newtabpage.activity-stream.showTopSites", false);
+user_pref("browser.newtabpage.activity-stream.topSitesCount", 12);
